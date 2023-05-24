4d28edd6703e8cf7c224ac24c327bb9e7c789179
Michal Mocny <mmocny@gmail.com>
2023-05-23 21:53:57 -0400

    revert accidental change

--- index.js
+++ index.js
@@ -3,14 +3,7 @@ import { blockFor } from "./workshop/utils/blockFor.js";
 const score = document.querySelector('score-keeper');
 const button = score.button;
 
-// button.addEventListener("click", () => {
-//   // blockFor(1000);
-//   score.incrementAndUpdateUI();
-// });
-
 button.addEventListener("click", () => {
+  // blockFor(1000);
   score.incrementAndUpdateUI();
-  requestAnimationFrame(() => {
-      blockFor(1000);
-  })
 });
